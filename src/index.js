c8a8f9102829635b9cf2ab86cf0a5d0b7ebd7cae
LauraTopacio25 <ltopacio@unitedvirtualities.com>
2021-03-19 11:28:21 -0700

    lista de usuarios

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ app.get('/', (request, response) => {
   response.send('Hello World!')
 })
 
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
 
 app.listen(3000,function(){console.log('server is listening')})
